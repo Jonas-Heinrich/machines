d0e9dadf32923d60c1c762ed93c1711b42b9ac83
Jonas Heinrich <jonas@jonasheinrich.com>
2019-11-15 21:14:08 +0100

    Add install script for vagrant

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb -O tmp/vagrant.deb
+sudo apt-get install ./tmp/vagrant.deb
+sudo chown -R $USER ~/.vagrant.d/
